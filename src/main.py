803e1c3121091a8ac601c965f99c5dc1643e98be
temirkan <tboziev@gmail.com>
2024-03-18 12:53:01 +0300

    main ok

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from utils import load_json, unpack
+import os
+from config import ROOT_DIR
+
+PATH_TO_PROD = os.path.join(ROOT_DIR, 'src', 'products.json')
+
+
+def main():
+    list_with_info = load_json(PATH_TO_PROD)
+    category_list, products_list = unpack(list_with_info)
+    print(category_list)
+    print(products_list)
+
+
+if __name__ == '__main__':
+    main()
