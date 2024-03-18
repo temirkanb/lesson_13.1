d825d2c5e94587e8a03b7fbc2d92d58ba21eb153
temirkan <tboziev@gmail.com>
2024-03-18 13:24:49 +0300

    ok ok

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,7 +1,7 @@
 from src.category import Category
 
 
-def test_init(coll):
+def test_initial(coll):
     assert coll.name == "Ноутбуки"
     assert coll.overview == ('Ноутбук, для работы и повседневной жизни, подходит для пользования почти всех '
                              'возрастов, можно смотреть видео, играть в игры, работать не выходя из дома')
