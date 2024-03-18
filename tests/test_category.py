df3132885a9ba07fc94e34c16496327a648997f1
temirkan <tboziev@gmail.com>
2024-03-18 13:23:36 +0300

    TEST_CATEGORY ok

---
+++ tests/test_category.py
@@ -0,0 +1,10 @@
+from src.category import Category
+
+
+def test_init(coll):
+    assert coll.name == "Ноутбуки"
+    assert coll.overview == ('Ноутбук, для работы и повседневной жизни, подходит для пользования почти всех '
+                             'возрастов, можно смотреть видео, играть в игры, работать не выходя из дома')
+    assert coll.goods[0].name == "Lenovo Think T15p"
+    assert Category.category_count == 1
+    assert Category.products == 5
