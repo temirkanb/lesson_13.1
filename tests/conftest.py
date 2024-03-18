a767849e6568d382a521c5cc4e6fbfa0877e2432
temirkan <tboziev@gmail.com>
2024-03-18 13:23:54 +0300

    conftest ok

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+import os
+from config import ROOT_DIR
+from src.product import Product
+from src.category import Category
+
+
+@pytest.fixture
+def for_test_load_json():
+    return os.path.join(ROOT_DIR, 'src', 'products.json')
+
+
+@pytest.fixture
+def coll():
+    return Category("Ноутбуки",
+                    "Ноутбук, для работы и повседневной жизни, подходит для пользования почти всех возрастов, "
+                    "можно смотреть видео, играть в игры, работать не выходя из дома",
+                    [Product("Lenovo Think T15p",
+                             "ssd - 1TB, Черный, Процессор Intel® Core, Вес - 2.5кг",
+                             93000.0, 8),
+                     Product(
+                         "MacBook Air 15", "ssd - 2TB, Полуночный черный,"
+                                           " Процессор - Apple M2 (8-Core CPU, 10-Core GPU), Вес - 1.5кг",
+                         290000.0, 6),
+                     Product("Thunderobot 911 M G3 Pro",
+                             "ssd - 512МB, Игровой, Процессор Intel Core i5 13500H 2.6 ГГц Серый, Вес - 2.1кг",
+                             109000.0, 10)])
